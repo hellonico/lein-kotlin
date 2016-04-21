dfd3e0269fc104263ee89ac85a9c09c68de20a3d
mark <mark@mark.mark>
2016-04-21 14:17:21 -0500

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject lein-kotlin "0.0.1"
+  :description "A Leiningen plugin to compile Kotlin code."
+  :url "https://github.com/reutermj/lein-kotlin"
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
+  :eval-in-leiningen true)
---
+++ src/leiningen/kotlin.clj
@@ -0,0 +1,18 @@
+(ns leiningen.kotlin
+  (:require [leiningen.core.eval :refer [eval-in-project]]
+            [leiningen.core.project :refer [merge-profiles]]))
+
+(defn kotlin
+  "I don't do a lot."
+  [project & args]
+  (let [ksrc (:kotlin-source-path project)
+        target (or (:compile-path project) "target")
+        version (or (:kotlin-compiler-version) "1.0.1-2")
+        p (merge-profiles project [{:dependencies [['org.jetbrains.kotlin/kotlin-compiler version]]}])
+        p (dissoc p :prep-tasks)]
+    (when ksrc
+      (eval-in-project
+        p
+        `(do
+           (org.jetbrains.kotlin.cli.jvm.K2JVMCompiler/main
+             (into-array ["-cp" (System/getProperty "java.class.path") "-d" ~target ~ksrc])))))))
