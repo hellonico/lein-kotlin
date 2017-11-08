f3ef2bfb44aef3d428e37071084dbe846668e45f
Nicolas Modrzyk <hellonico@gmail.com>
2017-11-08 09:47:37 +0900

    yeehaaaaaa

--- src/leiningen/kotlin.clj
+++ src/leiningen/kotlin.clj
@@ -8,7 +8,7 @@
   (let [ksrc (:kotlin-source-path project)
         target (or (:compile-path project) "target")
         jvmTarget (or (:kotlin-java-version project) "1.8")
-        version (or (:kotlin-compiler-version project) "1.1.4-3")
+        version (or (:kotlin-compiler-version project) "1.1.51")
         p (merge-profiles project [{:dependencies [['org.jetbrains.kotlin/kotlin-compiler version]]}])
         p (dissoc p :prep-tasks)]
     (when ksrc
@@ -16,5 +16,5 @@
         p
         `(do
            (org.jetbrains.kotlin.cli.jvm.K2JVMCompiler/main
-             ; (into-array ["jvmTarget" jvmTarget "-cp" (System/getProperty "java.class.path") "-d" ~target ~ksrc])))))))
-             (into-array [ "-no-stdlib" "-Xskip-runtime-version-check" "-cp" (System/getProperty "java.class.path") "-d" ~target ~ksrc])))))))
+             ; (into-array ["-jvmTarget" jvmTarget "-cp" (System/getProperty "java.class.path") "-d" ~target ~ksrc])))))))
+             (into-array ["-Xskip-runtime-version-check" "-no-stdlib" "-Xskip-runtime-version-check" "-cp" (System/getProperty "java.class.path") "-jvm-target" "1.8" "-d" ~target ~ksrc])))))))
